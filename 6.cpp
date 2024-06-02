4ae371955725c7c01531d063640045b8198963e7
Ilina Alina <ilinaalina>
2024-06-03 03:01:04 +0400

    Содание структуры для идеально сбалансированного дерева

---
+++ 6.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <fstream>
+#include <cmath>
+#include <queue>
+using namespace std;
+ifstream in("input.txt");
+struct tree {
+    int inf;
+    tree* right;
+    tree* left;
+};
+
+tree* node(int x) {
+    tree* n = new tree;
+    n->inf = x;
+    n->left = n->right = NULL;
+    return n;
+}
+void create(tree*& tr, int n) {
+    int x;
+    if (n > 0) {
+        in >> x;
+        tr = node(x);
+        int nl = n / 2;
+        int nr = n - nl - 1;
+        create(tr->left, nl);
+        create(tr->right, nr);
+    }
+}
+
+int main() {
+    setlocale(LC_ALL, "Ru");
+    tree* tr = NULL;
+    int n, x;
+    in >> n;
+    create(tr, n);
+    cout << endl;
+    return 0;
+}
